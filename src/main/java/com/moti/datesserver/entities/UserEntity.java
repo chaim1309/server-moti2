457c7c887a5e23ee6fddfc7873ff9151676470ce
Moti Raz <razmoti10@gmail.com>
2023-05-10 18:35:49 +0300

    Added data for entity

--- src/main/java/com/moti/datesserver/entities/UserEntity.java
+++ src/main/java/com/moti/datesserver/entities/UserEntity.java
@@ -14,34 +14,31 @@ public class UserEntity {
     private long id;
     @Column(name = "user_name", nullable = false, unique = true)
     private String userName;
-
     @Enumerated(EnumType.STRING)
     @Column(name = "user_type", nullable = false)
     private UserType userType;
-
     @Column(name = "password", nullable = false)
     private String password;
-
     @Column(name = "phone_number", nullable = false)
     private String poneNumber;
-
     @Column(name = "gender", nullable = false)
     private Gender gender;
-
-    @Column(name="name",nullable = false)
-    private String name;
+    @Column(name="first_Name",nullable = false)
+    private String firstName;
+    @Column(name="last_Name",nullable = false)
+    private String lastName;
     @Column(name="date_of_birth",nullable = false)
-    private int dateOfBirth;
-    @Column(name="describe_you" ,nullable = false)
-    private String describeYou;
-    @Column(name="what_to_see" ,nullable = false)
-    private WhatToSee whatToSee;
+    private  Date dateOfBirth;
+    @Column(name="information_About_Myself" ,nullable = false)
+    private String informationAboutMyself;
+    @Column(name="i_am_looking_for" ,nullable = false)
+    private Gender  lookingFor;
     @Column(name="introduction_purpose",nullable = false)
-    private String introductionPurpose;
+    private MeetingPurpose introductionPurpose;
 //vdvd
-    public UserEntity(long id, String userName, UserType userType, String password, String poneNumber,
-                      Gender gender, String name, int dateOfBirth,
-                      String describeYou, WhatToSee whatToSee, String introductionPurpose) {
+
+
+    public UserEntity(long id, String userName, UserType userType, String password, String poneNumber, Gender gender, String name, String name1, Date dateOfBirth, String informationAboutMyself, Gender lookingFor, MeetingPurpose introductionPurpose) {
         this.id = id;
         this.userName = userName;
         this.userType = userType;
@@ -49,9 +46,10 @@ public class UserEntity {
         this.poneNumber = poneNumber;
         this.gender = gender;
         this.name = name;
+        this.name = name1;
         this.dateOfBirth = dateOfBirth;
-        this.describeYou = describeYou;
-        this.whatToSee = whatToSee;
+        this.informationAboutMyself = informationAboutMyself;
+        this.lookingFor = lookingFor;
         this.introductionPurpose = introductionPurpose;
     }
 
@@ -114,35 +112,35 @@ public class UserEntity {
         this.name = name;
     }
 
-    public int getDateOfBirth() {
+    public Date getDateOfBirth() {
         return dateOfBirth;
     }
 
-    public void setDateOfBirth(int dateOfBirth) {
+    public void setDateOfBirth(Date dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
-    public String getDescribeYou() {
-        return describeYou;
+    public String getInformationAboutMyself() {
+        return informationAboutMyself;
     }
 
-    public void setDescribeYou(String describeYou) {
-        this.describeYou = describeYou;
+    public void setInformationAboutMyself(String informationAboutMyself) {
+        this.informationAboutMyself = informationAboutMyself;
     }
 
-    public WhatToSee getWhatToSee() {
-        return whatToSee;
+    public Gender getLookingFor() {
+        return lookingFor;
     }
 
-    public void setWhatToSee(WhatToSee whatToSee) {
-        this.whatToSee = whatToSee;
+    public void setLookingFor(Gender lookingFor) {
+        this.lookingFor = lookingFor;
     }
 
-    public String getIntroductionPurpose() {
+    public MeetingPurpose getIntroductionPurpose() {
         return introductionPurpose;
     }
 
-    public void setIntroductionPurpose(String introductionPurpose) {
+    public void setIntroductionPurpose(MeetingPurpose introductionPurpose) {
         this.introductionPurpose = introductionPurpose;
     }
 
